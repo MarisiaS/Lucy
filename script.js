c5e1e888b3afc3c985ca1c3a5f05d0156c0987a3
Elzbieta Sanjuan <marisiasanjuan@gmail.com>
2024-01-13 20:03:03 -0800

    Added basic funcionallity

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const textInputField = document.querySelector("#text-input")
+const form = document.querySelector("#form")
+const clean = document.querySelector("#clean-button")
+const utterThis = new SpeechSynthesisUtterance()
+const synth = window.speechSynthesis
+let ourText = ""
 
+const checkBrowserCompatibility = () => {
+  "speechSynthesis" in window
+    ? console.log("Web Speech API supported!")
+    : console.log("Web Speech API not supported :-(")
+}
+
+checkBrowserCompatibility()
+textInputField.focus();
+form.onsubmit = (event) => {
+    event.preventDefault()
+    ourText = textInputField.value
+    utterThis.text = ourText
+    synth.speak(utterThis)
+    textInputField.focus()
+  }
+
+function cleanText() {
+    document.getElementById("text-input").value = "";
+}
